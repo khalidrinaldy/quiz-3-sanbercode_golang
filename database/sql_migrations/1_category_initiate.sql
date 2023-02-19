d1be6739f850b1a5a7b9dff3068f7d6ce1690a5c
Macbook <mac@Macbooks-MacBook-Air-2.local>
2023-02-19 17:47:18 +0700

    test3

--- database/sql_migrations/1_category_initiate.sql
+++ database/sql_migrations/1_category_initiate.sql
@@ -4,8 +4,8 @@
 create table category (
     id serial not null primary key,
     name varchar(256),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+--     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
 
 -- +migrate StatementEnd
